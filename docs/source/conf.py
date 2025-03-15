3700e342f00b5695582c27632fb9b6fc90e0ca6f
yang-z. <xornent@outlook.com>
2025-03-15 23:02:56 +0800

    0.1.0 documentation commit 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../../scalign"))
+sys.path.insert(0, os.path.abspath("../.."))
 
 project = 'scalign'
 copyright = '2025, Zheng Yang'
